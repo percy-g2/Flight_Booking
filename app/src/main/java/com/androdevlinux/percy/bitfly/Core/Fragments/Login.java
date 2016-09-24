30d5752ad4c61830f4f0a781d83c3db339e307f1
percy-g2 <gahlotpercy@gmail.com>
2016-09-25 00:27:24 +0530

    BitFly
    initialize edittext and buttons onCreate of view for login

--- app/src/main/java/com/androdevlinux/percy/bitfly/Core/Fragments/Login.java
+++ app/src/main/java/com/androdevlinux/percy/bitfly/Core/Fragments/Login.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.androdevlinux.percy.bitfly.R;
 
@@ -17,14 +19,24 @@ public class Login extends Fragment {
 
     private View rootView;
 
+    private static final String TAG = "LoginActivity";
+
+    private static final int REQUEST_SIGNUP = 0;
+
+    //EditText
+    private EditText input_email;
+    private EditText input_password;
+
+    //Button
+    private Button btn_login;
+    private Button btn_login_google;
+    private Button btn_login_facebook;
+
     public Login () {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
     }
 
     @Override
@@ -32,6 +44,13 @@ public class Login extends Fragment {
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rootView = inflater.inflate(R.layout.login, container, false);
+
+        input_email = (EditText) rootView.findViewById(R.id.input_email);
+        input_password = (EditText) rootView.findViewById(R.id.input_password);
+
+        btn_login = (Button) rootView.findViewById(R.id.btn_login);
+        btn_login_google = (Button) rootView.findViewById(R.id.btn_login_google);
+        btn_login_facebook = (Button) rootView.findViewById(R.id.btn_login_facebook);
 
         return rootView;
     }
